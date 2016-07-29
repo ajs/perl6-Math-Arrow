db49a1550423634e638a8bdb23ccf65513ef9a47
Aaron Sherman <ajs@ajs.com>
2016-07-29 15:06:36 -0400

    Added function version arrow()

--- lib/Math/Arrow.pm
+++ lib/Math/Arrow.pm
@@ -10,5 +10,12 @@ sub infix:<↑↑>($a, $b) is assoc<right> is export { [↑] $a xx $b }
 sub infix:<↑↑↑>($a, $b) is assoc<right> is export { [↑↑] $a xx $b }
 sub infix:<↑↑↑↑>($a, $b) is assoc<right> is export { [↑↑↑] $a xx $b }
 sub infix:<↑↑↑↑↑>($a, $b) is assoc<right> is export { [↑↑↑↑] $a xx $b }
+sub arrow(Cool $a, Cool $b where { $b >= 2 and $b.Int == $b }, Cool $arrows where {$arrows.Int == $arrows}) is export {
+    die "Negative arrows not allowed" if $arrows < 0;
+    return ($a ↑ $b) if $arrows <= 1 or $b <= 1;
+    # Technically we should reverse this because of associativity,
+    # but since the power tower are all identical...
+    ($a xx $b).reduce: -> $x, $y { arrow($y, $x, $arrows-1) };
+}
 
 # vim: sw=4 softtabstop=4 ai expandtab filetype=perl6
--- t/arrow.t
+++ t/arrow.t
@@ -5,9 +5,12 @@ use v6;
 use Test;
 use Math::Arrow;
 
-plan(2);
+plan(5);
 
-is 3↑3, 27, "Exponentiation";
-is 3↑↑3, 7625597484987, "Two arrow power tower";
+is 3 ↑ 3, 27, "Exponentiation";
+is 3 ↑↑ 3, 7625597484987, "Two arrow power tower";
+is arrow(3,3,2), 3 ↑↑ 3, "arrow(...) function form";
+is 3 ↑↑↑ 2, 3 ↑↑ 3, "Triple arrow";
+is 2 ↑↑↑ 2, 4, "Simple ²2 = 4";
 
 # vim: sw=4 softtabstop=4 ai expandtab filetype=perl6
